18c7400d0a80f07767fa188ba2614a8dbb3ff3a9
byjiwon <byjiwon2@gmail.com>
2020-01-02 18:54:00 +0900

    Add navigator

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import TabNavigation from "./navigation/TabNavigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => {
+    console.log(error);
+  };
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return <TabNavigation />;
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onError={this.handleError}
+          onFinish={this.handleLoaded}
+        />
+      );
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- READEME.md
+++ READEME.md
@@ -1,3 +1,10 @@
 # Nomad Movie
 
 Native Movie App ver 1.0
+
+## Screens
+
+- [ ] Movies
+- [ ] TV shows
+- [ ] Search
+- [ ] Detail
